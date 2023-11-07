c90f86a009f69b91a640f769c0a6512c05158557
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-11-07 14:07:27 +0100

    Make the created nix.conf customizable

---
+++ .github/workflows/test.nix
@@ -0,0 +1,15 @@
+let pkgs = import <nixpkgs> {};
+in {
+  alwaysNewDerivation = pkgs.writeText "build-example" (builtins.toString builtins.currentTime);
+  constantDerivation = pkgs.runCommand "fetch-example" {} "echo Example > $out";
+  recursiveNixDerivation = pkgs.runCommand "recursive-nix-example"
+    {
+      buildInputs = [ pkgs.nix ];
+      requiredSystemFeatures = [ "recursive-nix" ];
+    }
+    ''
+      mkdir $out
+      echo ${builtins.toString builtins.currentTime} > $out/time.txt
+      ln -s $(nix-build ${./test.nix} -A constantDerivation) $out/recursive-output
+    '';
+}
